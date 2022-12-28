a803a9ae5ae6f6b8fbfc1eda00c77b92d08fe356
adzhigitovruslan <adzhigitovruslan@gmail.com>
2022-12-28 21:50:28 +0800

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+	chainWebpack: config => {
+        config.module.rules.delete('eslint');
+    },
+	css: {
+	loaderOptions: {
+		sass: {
+		prependData: `@import "@/assets/scss/null.scss";`
+		}
+	}
+	}
+  };
